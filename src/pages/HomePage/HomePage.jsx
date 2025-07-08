ba69b0b5b24b0fc920467536956101afd9b3234d
David Franklin D. <davidfranklin1999@gmail.com>
2025-07-08 22:18:28 +0530

    feat: add homepage and footer

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Hero from '../../components/Hero/Hero'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Hero />
+    </div>
   )
 }
+
