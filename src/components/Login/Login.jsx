c37cbfce1ffa6e82b698603b094d79a9623407c1
David Franklin D. <davidfranklin1999@gmail.com>
2025-07-08 20:20:05 +0530

    fix: for --htmlFor, autocomplete --autoComplete; valid in plain HTML but not in JSX

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -25,16 +25,16 @@ const Login = () => {
   <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form className="space-y-4" action="#" method="POST">
       <div>
-        <label for="email" className="block text-sm/6 font-medium text-gray-900">Email address</label>
+        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">Email address</label>
         <div className="mt-2">
-          <input type="email" name="email" id="email" value={data.email} onChange={handleChange} autocomplete="email" required className="block w-full border border-gray-300 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input type="email" name="email" id="email" value={data.email} onChange={handleChange} autoComplete="email" required className="block w-full border border-gray-300 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
       </div>
 
       <div> 
-        <label for="password" className="block text-sm/6 font-medium text-gray-900">Password</label>
+        <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">Password</label>
         <div className="mt-2">
-          <input type="password" name="password" id="password" value={data.password} autocomplete="current-password" required className="border border-gray-300 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input type="password" name="password" id="password" value={data.password} autoComplete="current-password" required className="border border-gray-300 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
       </div>
 
